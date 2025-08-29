e6b423d897eff4b03f7940169f1ce4643d57f865
Isaque <isaqueaf9307286@gmail.com>
2025-08-29 16:49:47 -0300

    corrigindo barra de pesquisa para diferentes padrões de tela

--- src/script/index.js
+++ src/script/index.js
@@ -1,49 +1,32 @@
+// Search bar
 const searchBar = document.querySelector('.search__bar');
+const icon = document.querySelector('.search__bar i')
+const inputForm = document.querySelector('.search__bar form');
 const inputQuery = document.querySelector('.search__bar form input');
 
-const CPFMask = [
-  [4, "."],
-  [8, "."],
-  [12, "-"],
-]
-
-function mask (values, textElement, input, end, number) {
-  if (number) {
-    number = Number(input.data)
-  }
-  if (isNaN(number) || input.data == " ") {
-    textElement.value = textElement.value.slice(0, -1)
-  }
-  values.forEach(value => {
-    if (textElement.value.length === end + 1) {
-      textElement.value = textElement.value.slice(0, -1)
-    }
-    if (textElement.value.length === value[0]) {
-      textElement.value = textElement.value.slice(0, -1)
-      textElement.value += value[1]
-      if (input.data) {
-        textElement.value += input.data
-      }
-    }
-    if (textElement.value.length === value[0] + value[1].length - 1 && input.data === null) {
-      textElement.value = textElement.value.slice(0, -value[1].length)
-    }
-  });
-}
-
-searchBar.addEventListener('click', () => {
-  inputQuery.focus();  
-  inputQuery.computedStyleMap.color = "red";
+icon.addEventListener('click', () => {
+  console.log('not__focused')
+  inputForm.classList.remove('not__focused');
+  inputQuery.classList.remove('not__focused');
+  searchBar.classList.add('focused');
+  inputQuery.focus();
 })
 
 inputQuery.addEventListener('focus', () => {
-  searchBar.classList.add("hover__search__bar");
+  searchBar.classList.add('hover__search__bar');
 });
 
 inputQuery.addEventListener('blur', () => {
-  searchBar.classList.remove("hover__search__bar");
+  searchBar.classList.remove('hover__search__bar');
+  inputForm.classList.add('not__focused');
+  inputQuery.classList.add('not__focused');
+  searchBar.classList.remove('focused');
 });
 
-inputQuery.addEventListener("input", input => {
-  mask(CPFMask, inputQuery, input, 14, false)
+// Menu
+const menuButton = document.getElementById('menu__toggle');
+const menu = document.querySelector('.menu__container');
+
+menuButton.addEventListener('click', PointerEvent => {
+  menu.classList.toggle('active');
 })
