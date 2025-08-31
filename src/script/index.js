4da8c352766e960c2abcfbe3b02231feb73f6102
IsaqueAF <isaqueaf9307286@gmail.com>
2025-08-30 21:11:05 -0300

    correção da search bar

--- src/script/index.js
+++ src/script/index.js
@@ -1,32 +1,45 @@
 // Search bar
-const searchBar = document.querySelector('.search__bar');
-const icon = document.querySelector('.search__bar i')
-const inputForm = document.querySelector('.search__bar form');
-const inputQuery = document.querySelector('.search__bar form input');
+const searchBar = document.querySelector(".search__bar");
+const inputForm = document.querySelector(".search__bar form");
+const inputQuery = document.querySelector(".search__bar form input");
+const mobileButtons = document.querySelector(".mobile__buttons");
+const navContainer = document.querySelector(".nav__container");
 
-icon.addEventListener('click', () => {
-  console.log('not__focused')
-  inputForm.classList.remove('not__focused');
-  inputQuery.classList.remove('not__focused');
-  searchBar.classList.add('focused');
+const breakPoint = 650;
+
+function moveElements() {
+  if (window.innerWidth <= 650) {
+    if (searchBar.parentElement == mobileButtons) {
+      return;
+    }
+    mobileButtons.prepend(searchBar);
+  } else {
+    if (searchBar.parentElement == navContainer) {
+      return;
+    }
+    navContainer.append(searchBar);
+  }
+}
+
+window.addEventListener("resize", moveElements);
+
+moveElements();
+
+searchBar.addEventListener("click", () => {
+  searchBar.classList.add("focused");
+  inputForm.classList.remove("not__focused");
   inputQuery.focus();
-})
-
-inputQuery.addEventListener('focus', () => {
-  searchBar.classList.add('hover__search__bar');
 });
 
-inputQuery.addEventListener('blur', () => {
-  searchBar.classList.remove('hover__search__bar');
-  inputForm.classList.add('not__focused');
-  inputQuery.classList.add('not__focused');
-  searchBar.classList.remove('focused');
+inputQuery.addEventListener("blur", () => {
+  searchBar.classList.remove("focused");
+  inputForm.classList.add("not__focused");
 });
 
 // Menu
-const menuButton = document.getElementById('menu__toggle');
-const menu = document.querySelector('.menu__container');
+const menuButton = document.getElementById("menu__toggle");
+const menu = document.querySelector(".menu__container");
 
-menuButton.addEventListener('click', PointerEvent => {
-  menu.classList.toggle('active');
-})
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
