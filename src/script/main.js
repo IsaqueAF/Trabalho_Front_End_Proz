86301692e68d124ab3ca60b38badfd49e92419d4
IsaqueAF <isaqueaf9307286@gmail.com>
2025-09-02 06:50:40 -0300

    correção de bugs e adição de register e login

--- src/script/main.js
+++ src/script/main.js
@@ -9,9 +9,16 @@ const accountContainer = document.querySelector(".account__container");
 const mobileButtons = document.querySelector(".mobile__buttons");
 const menu = document.querySelector(".menu__container");
 
+const inputForm = document.querySelector(".search__bar form");
+const inputQuery = document.querySelector(".search__bar form input");
+
+const menuButton = document.getElementById("menu__toggle");
+
 const breakPoint = 650;
 
 let changed = false;
+let change = false;
+
 let offscreenHeader = false;
 let searchBarFocused = false;
 
@@ -34,20 +41,33 @@ function moveElements() {
     if (!changed) {
       return;
     }
+
     mobileButtons.classList.remove("hidden");
+    if (!inputQuery.classList.contains("shrunken")) {
+      change = true;
+    }
     mobileButtons.prepend(searchBar);
+
     menu.insertBefore(nav, menu.children[1]);
     menu.append(accountContainer);
+
     changed = !changed;
   } else {
     if (changed) {
       return;
     }
+
     mobileButtons.classList.add("hidden");
-    menu.classList.add("hidden");
+    menu.classList.add("menu__hidden");
+
     header.insertBefore(nav, header[1]);
+    if (!inputQuery.classList.contains("shrunken")) {
+      change = true;
+    }
     navContainer.append(searchBar);
+
     header.append(accountContainer);
+
     changed = !changed;
   }
 }
@@ -55,15 +75,18 @@ function moveElements() {
 window.addEventListener("resize", moveElements);
 window.addEventListener("scroll", moveElements);
 
-moveElements();
+window.addEventListener("hashchange", () => {
+  moveElements();
+});
 
 // Search bar
-const inputForm = document.querySelector(".search__bar form");
-const inputQuery = document.querySelector(".search__bar form input");
 
 searchBar.addEventListener("click", () => {
   searchBar.classList.add("focused");
   inputForm.classList.remove("hidden");
+  setTimeout(() => {
+    inputQuery.classList.remove("shrunken");
+  }, 10);
   inputQuery.focus();
   if (!offscreenHeader) {
     searchBar.classList.add("hover");
@@ -72,18 +95,27 @@ searchBar.addEventListener("click", () => {
 });
 
 inputQuery.addEventListener("blur", () => {
-  searchBar.classList.remove("focused");
-  searchBar.classList.remove("hover");
-  inputForm.classList.add("hidden");
-  searchBarFocused = false;
+  if (change) {
+    change = false;
+    setTimeout(() => {
+      inputQuery.focus();
+    }, 10);
+  } else {
+    inputQuery.classList.add("shrunken");
+    setTimeout(() => {
+      inputForm.classList.add("hidden");
+      searchBar.classList.remove("focused");
+      searchBar.classList.remove("hover");
+      searchBarFocused = false;
+    }, 510);
+  }
 });
 
 // Menu
-const menuButton = document.getElementById("menu__toggle");
 
 document.addEventListener("click", (event) => {
   if (menuButton.contains(event.target)) {
-    menu.classList.toggle("hidden");
+    menu.classList.toggle("menu__hidden");
     return;
   }
   if (
@@ -92,6 +124,6 @@ document.addEventListener("click", (event) => {
     event.target.tagName == "I" ||
     event.target.tagName == "SPAN"
   ) {
-    menu.classList.add("hidden");
+    menu.classList.add("menu__hidden");
   }
 });
